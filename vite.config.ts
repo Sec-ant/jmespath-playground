d18ed764ac6e65089b96a21583a9d91be5f0981b
Ze-Zheng Wu <zezhengwu@proton.me>
2025-04-04 22:55:13 +0800

    feat: add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
 });
